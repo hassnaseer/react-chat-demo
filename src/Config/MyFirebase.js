9edd02b2c221d60e5dadd0ca48b53546584891ca
hassam nasser <muhammad.hassam@argonteq.com>
2023-04-19 14:00:29 +0500

    first commit

--- src/Config/MyFirebase.js
+++ src/Config/MyFirebase.js
@@ -1,12 +1,13 @@
 import firebase from 'firebase'
 
 const config = {
-    apiKey: 'AIzaSyAC9ZSyvTxU2o5W0jcL0hDvzS4utLuicE8',
-    authDomain: 'flutterchatdemo.firebaseapp.com',
-    databaseURL: 'https://flutterchatdemo.firebaseio.com',
-    projectId: 'flutterchatdemo',
-    storageBucket: 'flutterchatdemo.appspot.com',
-    messagingSenderId: '347976604232'
+    apiKey: "AIzaSyBJWt1Yh6AufjxV8B8Y8UVz_25cYV1fvhs",
+    authDomain: "joblinxs-9ecf3.firebaseapp.com",
+    projectId: "joblinxs-9ecf3",
+    storageBucket: "joblinxs-9ecf3.appspot.com",
+    messagingSenderId: "152202356320",
+    appId: "1:152202356320:web:9ca19a6584e100ab4a8505",
+    measurementId: "G-PLE4TBYC7N"
 }
 firebase.initializeApp(config)
 firebase.firestore().settings({
